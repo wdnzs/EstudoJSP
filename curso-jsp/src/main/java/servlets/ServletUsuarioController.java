2da9d12f5a13a30d5c173b918e86fcd71d692654
wdnzs <wilson.diniz@outlook.com.br>
2021-09-11 21:21:09 -0300

    Correção tela cadastro

--- curso-jsp/src/main/java/servlets/ServletUsuarioController.java
+++ curso-jsp/src/main/java/servlets/ServletUsuarioController.java
@@ -11,48 +11,39 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.ModelLogin;
 
-
 @WebServlet("/ServletUsuarioController")
 public class ServletUsuarioController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	private DAOUsuarioRepository daoUsuarioRepository = new DAOUsuarioRepository();
-	
-    public ServletUsuarioController() {
-        
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	private DAOUsuarioRepository daoUsuarioRepository = new DAOUsuarioRepository();
+
+	public ServletUsuarioController() {
+
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		try {
-			
-		String id = request.getParameter("id");
-		String nome = request.getParameter("nome");
-		String email = request.getParameter("email");
-		String login = request.getParameter("login");
-		String senha = request.getParameter("senha");
-		
-		
-		ModelLogin modelLogin = new ModelLogin();
-		
-		modelLogin.setId(id);
-		modelLogin.setNome(nome);
-		modelLogin.setEmail(email);
-		modelLogin.setLogin(login);
-		modelLogin.setSenha(senha);
-		
-		
-		daoUsuarioRepository.gravarUsuario(modelLogin);
-		
-		request.setAttribute("msg", "Operação realizada com sucesso!");
-		request.setAttribute("modolLogin", modelLogin);
-		request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
-		
-		}catch (Exception e){
+			String acao = request.getParameter("acao");
+
+			if (acao != null && !acao.isEmpty() && acao.equalsIgnoreCase("deletar")) {
+				
+				String idUser = request.getParameter("id");
+				daoUsuarioRepository.deletarUser(idUser);
+				request.setAttribute("msg", "Excluido com sucesso!");
+				request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
+
+			} else if (acao != null && !acao.isEmpty() && acao.equalsIgnoreCase("deletarajax")) {
+				
+				String idUser = request.getParameter("id");
+				daoUsuarioRepository.deletarUser(idUser);
+				response.getWriter().write("Excluido com sucesso!");
+				
+			} else {
+				request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
+			}
+
+		} catch (Exception e) {
 			e.printStackTrace();
 			RequestDispatcher redirecionar = request.getRequestDispatcher("erro.jsp");
 			request.setAttribute("msg", e.getMessage());
@@ -60,4 +51,48 @@ public class ServletUsuarioController extends HttpServlet {
 		}
 	}
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		try {
+
+			String msg = "Operação realizada com sucesso!";
+
+			String id = request.getParameter("id");
+			String nome = request.getParameter("nome");
+			String email = request.getParameter("email");
+			String login = request.getParameter("login");
+			String senha = request.getParameter("senha");
+
+			ModelLogin modelLogin = new ModelLogin();
+
+			modelLogin.setId(id != null && !id.isEmpty() ? Long.parseLong(id) : null);
+			modelLogin.setNome(nome);
+			modelLogin.setEmail(email);
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+
+			if (daoUsuarioRepository.validarLogin(modelLogin.getLogin()) && modelLogin.getId() == null) {
+				msg = "Já existe usuário com o mesmo login, informe outro login.";
+			} else {
+				if (modelLogin.isNovo()) {
+					msg = "Gravado com sucesso!";
+				} else {
+					msg = "Atualizado com sucesso!";
+				}
+
+				modelLogin = daoUsuarioRepository.gravarUsuario(modelLogin);
+			}
+
+			request.setAttribute("msg", msg);
+			request.setAttribute("modolLogin", modelLogin);
+			request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			RequestDispatcher redirecionar = request.getRequestDispatcher("erro.jsp");
+			request.setAttribute("msg", e.getMessage());
+			redirecionar.forward(request, response);
+		}
+	}
 }
